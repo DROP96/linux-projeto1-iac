f04f957d717e11aad01a0f932038f2b5bcbd2ccf
DROP96 <dede.david96@gmail.com>
2024-12-01 06:23:30 +0000

    Arquivos IaC V.1

--- iacl.sh
+++ iacl.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 echo "Criando diretórios..."
 
 mkdir /publico
@@ -8,8 +7,8 @@ mkdir /adm
 mkdir /ven
 mkdir /sec
 
-
 echo "Criando grupos de usuários..."
+
 
 groupadd GRP_ADM
 groupadd GRP_VEN
@@ -17,27 +16,42 @@ groupadd GRP_SEC
 
 echo "Criando usuários..."
 
-useradd Carlos -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_ADM
-useradd Maria -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_ADM
-useradd João -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_ADM
+# Criando usuários e atribuindo aos grupos
+useradd Carlos -m -s /bin/bash -G GRP_ADM
+useradd Maria -m -s /bin/bash -G GRP_ADM
+useradd João -m -s /bin/bash -G GRP_ADM
 
+useradd Debora -m -s /bin/bash -G GRP_VEN
+useradd Sebastiana -m -s /bin/bash -G GRP_VEN
+useradd Roberto -m -s /bin/bash -G GRP_VEN
 
-useradd Debora -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN
-useradd Sebastiana -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN
-useradd Roberto -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN
+useradd Josefina -m -s /bin/bash -G GRP_SEC
+useradd Amanda  -m -s /bin/bash -G GRP_SEC
+useradd Rogerio -m -s /bin/bash -G GRP_SEC
 
-useradd Josefina -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC
-useradd Amanda  -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC
-useradd Rogerio -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC
+echo "Definindo senhas dos usuários..."
 
+# Definindo senhas usando chpasswd
+echo "Carlos:senha123" | chpasswd
+echo "Maria:senha123" | chpasswd
+echo "João:senha123" | chpasswd
 
+echo "Debora:senha123" | chpasswd
+echo "Sebastiana:senha123" | chpasswd
+echo "Roberto:senha123" | chpasswd
+
+echo "Josefina:senha123" | chpasswd
+echo "Amanda:senha123" | chpasswd
+echo "Rogerio:senha123" | chpasswd
 
 echo "Especificando permissões dos diretórios..."
 
+# Definindo a propriedade dos diretórios
 chown root:GRP_ADM /adm
 chown root:GRP_VEN /ven
 chown root:GRP_SEC /sec
 
+# Definindo permissões dos diretórios
 chmod 770 /adm
 chmod 770 /ven
 chmod 770 /sec
